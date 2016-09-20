5b631ffc35693ce6b2778bb090db0cb32d67d55a
Jerry <jerry_shenchao@163.com>
2016-09-20 14:47:59 +0800

    account test

--- src/main/java/me/shenchao/mvnlearn/account/email/AccountEmailServiceImpl.java
+++ src/main/java/me/shenchao/mvnlearn/account/email/AccountEmailServiceImpl.java
@@ -1,8 +1,44 @@
 package me.shenchao.mvnlearn.account.email;
 
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 public class AccountEmailServiceImpl implements AccountEmailService {
 
-    public void sendMail(String to, String subject, String htmlText) throws AccountEmailException {
+    private JavaMailSender javaMailSender;
 
+    private String systemEmail;
+
+    public void sendMail(String to, String subject, String htmlText) throws AccountEmailException {
+        MimeMessage msg = javaMailSender.createMimeMessage();
+        MimeMessageHelper msgHelper = new MimeMessageHelper(msg);
+
+        try {
+            msgHelper.setFrom(systemEmail);
+            msgHelper.setTo(to);
+            msgHelper.setSubject(subject);
+            msgHelper.setText(htmlText ,true);
+        } catch (MessagingException e) {
+            throw new AccountEmailException("Failed to send mail");
+        }
+    }
+
+    public JavaMailSender getJavaMailSender() {
+        return javaMailSender;
+    }
+
+    public void setJavaMailSender(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public String getSystemEmail() {
+        return systemEmail;
+    }
+
+    public void setSystemEmail(String systemEmail) {
+        this.systemEmail = systemEmail;
     }
 }
